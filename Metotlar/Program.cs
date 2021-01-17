efd7f85c57eff53a81bd63e0710a62db7159ce87
muhammed konyalicetin <365068@ogr.ktu.edu.tr>
2021-01-17 20:51:41 +0300

    16.01.2021

---
+++ Metotlar/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UrunAdi = "Elma";
+            double Fiyati = 15;
+            string Aciklama = "Amasya Elması";
+
+
+            string[] meyveler = new string[] { };
+
+            Product urun1 = new Product();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Product urun2 = new Product();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Product[] urunler = new Product[] { urun1, urun2 };
+
+            foreach (Product urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("----------"); ;
+            }
+
+            Console.WriteLine("------------Metotlar-------------");
+
+            //instance
+            //encapsulation
+
+            SepetManager sepetManager = new SepetManager();
+
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+
+            sepetManager.Ekle2("Elma", "yeşil elma", 12,8);
+            sepetManager.Ekle2("armut", "yeşil armt", 12, 10);
+            sepetManager.Ekle2("karpuz", "diyarbakır", 12, 55);
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //naming convention
+        //syntax
+
+        public void Ekle(Product urun)
+        {
+            Console.WriteLine("Sepete Eklendi : " + urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int StokAdeti)
+        {
+            Console.WriteLine("Tebrikler sepete eklendi : " + urunAdi);
+        }
+
+    }
+}
